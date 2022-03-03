f9b793714f5dd8a2a38560c7961d19323b9abb3f
delfina <delfina.ghimire@gmail.com>
2022-03-03 18:12:38 +0545

    Single product page

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
-const hamburger = document.getElementById("bar");
-const navbar = document.getElementById("navbar");
-const close = document.getElementById("close");
+"use strict";
+let mainImg = document.getElementById("main-img");
+let smImg = document.querySelectorAll(".sm-img");
+let proItem = document.querySelectorAll(".product-item");
 
-hamburger.addEventListener("click", function () {
-  navbar.classList.add("show-navbar");
+proItem[0].addEventListener("click", function () {
+  window.location.href = "sproduct.html";
 });
 
-if (close) {
-  close.addEventListener("click", function () {
-    navbar.classList.remove("show-navbar");
+for (let i = 0; i <= smImg.length; i++) {
+  smImg[i].addEventListener("click", function () {
+    mainImg.src = smImg[i].src;
   });
 }
